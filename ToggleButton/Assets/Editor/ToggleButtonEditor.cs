eebbd50268a9b4a79add7bfd3531033f22544fdb
MasujimaRyohei <info@MasujimaRyohei.jp>
2018-09-28 17:13:10 +0900

    Initial commit

---
+++ ToggleButton/Assets/Editor/ToggleButtonEditor.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+/// <summary>
+/// Editor extensions.
+/// </summary>
+[CustomEditor(typeof(ToggleButton))]
+public class ToggleButtonEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        ToggleButton toggle = target as ToggleButton;
+
+        Undo.RecordObject(toggle, "record property");
+
+
+        toggle.Interactable = EditorGUILayout.Toggle("Interactable", toggle.Interactable);
+        toggle.IsOn = EditorGUILayout.Toggle("Is On", toggle.IsOn);
+
+        if (toggle.Interactable)
+            if (toggle.IsOn)
+                toggle.sprite = toggle.OnSprite;
+            else
+                toggle.sprite = toggle.OffSprite;
+        else
+            toggle.sprite = toggle.DisabledSprite;
+        
+        toggle.OnSprite = EditorGUILayout.ObjectField("On Sprite", toggle.OnSprite, typeof(Sprite), true) as Sprite;
+        toggle.OffSprite = EditorGUILayout.ObjectField("Off Sprite", toggle.OffSprite, typeof(Sprite), true) as Sprite;
+        toggle.DisabledSprite = EditorGUILayout.ObjectField("Disabled Sprite", toggle.DisabledSprite, typeof(Sprite), true) as Sprite;
+        GUILayout.Label("callback event [void onToggle(bool toggleValue)]", GUI.skin.box);
+
+        if (GUILayout.Button("SetNativeSize"))
+            toggle.SetNativeSize();
+    }
+}
---
+++ ToggleButton/Assets/Scripts/ToggleButton.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+/// <summary>
+/// uGUI toggle button.
+/// </summary>
+public class ToggleButton : Image, IPointerDownHandler, IPointerUpHandler, IDragHandler
+{
+    [SerializeField]
+    private Sprite onSprite;
+    public Sprite OnSprite
+    {
+        get { return onSprite; }
+        set { onSprite = value; }
+    }
+
+    [SerializeField]
+    private Sprite offSprite;
+    public Sprite OffSprite
+    {
+        get { return offSprite; }
+        set { offSprite = value; }
+    }
+
+    [SerializeField]
+    private Sprite disabledSprite;
+    public Sprite DisabledSprite
+    {
+        get { return disabledSprite; }
+        set { disabledSprite = value; }
+    }
+
+    [SerializeField]
+    private bool interactable;
+    public bool Interactable
+    {
+        get { return interactable; }
+        set
+        {
+            interactable = value;
+            if (value)
+                if (isOn)
+                    sprite = onSprite;
+                else
+                    sprite = offSprite;
+            else
+                sprite = disabledSprite;
+        }
+    }
+    [SerializeField]
+    private bool isOn;
+    public bool IsOn
+    {
+        get { return isOn; }
+        set { isOn = value; }
+    }
+
+    public delegate void ToggleDelegate(bool toggledValue);
+    public ToggleDelegate onToggle = delegate { };
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    protected override void Awake() => base.Awake();
+
+    /// <summary>
+    /// When tap down.
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerDown(PointerEventData eventData) { }
+
+    /// <summary>
+    /// When tap up. ( This function has to need IPointerDownHandler of interface. )
+    /// </summary>
+    /// <param name="eventData"></param>
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (!interactable)
+            return;
+
+        // For tap cancel.
+        if (eventData.pointerPressRaycast.gameObject == eventData.pointerCurrentRaycast.gameObject)
+            Toggle();
+    }
+
+    public void OnDrag(PointerEventData eventData){}
+
+    /// <summary>
+    /// Toggle button.
+    /// </summary>
+    public void Toggle()
+    {
+        isOn = !isOn;
+
+        if (isOn)
+            sprite = onSprite;
+        else
+            sprite = offSprite;
+        
+        onToggle(isOn);
+    }
+}
